1c4227306552381a8e3e275392791a238d2df75e
Tarak <tarak.chaari@gmail.com>
2012-10-20 12:00:48 +0100

    Switch to inline kernel (github.com/bedalus/marmite)

--- slim.mk
+++ slim.mk
@@ -9,19 +9,6 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 # Inherit device settings
 $(call inherit-product, vendor/slim/config/common_nexus.mk)
 
-#copy kernel and modules
-PRODUCT_COPY_FILES += \
-	device/samsung/crespo4g/prebuilt/boot.img:system/slimkernel/boot.img \
-	device/samsung/crespo4g/prebuilt/system/etc/init.d/98mounts:system/etc/init.d/98mounts \
-	device/samsung/crespo4g/prebuilt/system/lib/libOMX.SEC.AVC.Decoder.so:system/lib/libOMX.SEC.AVC.Decoder.so \
-	device/samsung/crespo4g/prebuilt/system/lib/libOMX.SEC.AVC.Encoder.so:system/lib/libOMX.SEC.AVC.Encoder.so \
-	device/samsung/crespo4g/prebuilt/system/lib/libOMX.SEC.M4V.Decoder.so:system/lib/libOMX.SEC.M4V.Decoder.so \
-	device/samsung/crespo4g/prebuilt/system/lib/libOMX.SEC.M4V.Encoder.so:system/lib/libOMX.SEC.M4V.Encoder.so \
-	device/samsung/crespo4g/prebuilt/system/lib/libSEC_OMX_Core.so:system/lib/libSEC_OMX_Core.so \
-	device/samsung/crespo4g/prebuilt/system/lib/libstagefright_omx.so:system/lib/libstagefright_omx.so \
-	device/samsung/crespo4g/prebuilt/system/lib/hw/lights.s5pc110.so:system/lib/hw/lights.s5pc110.so \
-	device/samsung/crespo4g/prebuilt/system/modules/scsi_wait_scan.ko:system/modules/scsi_wait_scan.ko
-
 #bootanimation
 PRODUCT_COPY_FILES +=  \
     vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
